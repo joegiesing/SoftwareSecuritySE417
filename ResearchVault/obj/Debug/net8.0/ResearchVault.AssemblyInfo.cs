d86a06ff3fdf8169c582fec3fe0de34ddc2de6fb
Salvanim <109810280+Salvanim@users.noreply.github.com>
2026-02-21 16:08:59 -0500

    ignore assemblyinfo

--- ResearchVault/obj/Debug/net8.0/ResearchVault.AssemblyInfo.cs
+++ ResearchVault/obj/Debug/net8.0/ResearchVault.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ResearchVault")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5038b42c60c0cf503fcf8a904f281bc711baf47e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f18389af21e40534fd49c66c79670411f63f036d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ResearchVault")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ResearchVault")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
